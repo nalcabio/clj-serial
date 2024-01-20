eca4489269f89351cd98cf28e15b21d6c35bf569
Tom Vaughan <thomas.david.vaughan@gmail.com>
2024-01-20 15:39:21 -0300

    Bump clojure and purejavacomm, and release v2.0.6.

--- project.clj
+++ project.clj
@@ -1,11 +1,16 @@
-(defproject clj-serial "2.0.6-SNAPSHOT"
+(defproject bio.nalca/clj-serial "2.0.6"
   :description "Simple serial port library. Wraps PureJavaComm."
-  :url "https://github.com/peterschwarz/clj-serial"
+  :url "https://github.com/nalcabio/clj-serial"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.github.purejavacomm/purejavacomm "1.0.2.RELEASE"]]
-  :profiles {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [bio.nalca/purejavacomm "1.0.4"]]
+  :repositories [["bio.nalca/purejavacomm" {:url "https://maven.pkg.github.com/nalcabio/purejavacomm"
+                                            :username :env/GITHUB_USERNAME
+                                            :password :env/GITHUB_PASSWORD}]]
+  :deploy-repositories [["releases" {:url "https://maven.pkg.github.com/nalcabio/clj-serial"
+                                     :username :env/GITHUB_USERNAME
+                                     :password :env/GITHUB_PASSWORD}]]
+  :profiles {:1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}}
   :scm {:name "git"
-        :url "https://github.com/peterschwarz/clj-serial"})
+        :url "https://github.com/nalcabio/clj-serial"})
